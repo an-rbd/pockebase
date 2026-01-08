8f5a3c11d767fbbd4427e959a8c14a19afbb18c0
an-rbd <angierobergeaud01@gmail.com>
2026-01-08 16:12:06 +0100

    maj

--- auto.sh
+++ auto.sh
@@ -4,16 +4,16 @@ IP_BACKEND="172.24.140.14"
 IP_FRONT="172.24.138.203"
 git clone https://github.com/an-rbd/pockebase.git
 
-cd /pockebase/bash
+
 chmod +x *.sh
 set -e
 cd pockebase
 echo "Install BACKEND"
-ssh user@$IP_BACKEND -p 4222 'bash -s' < ./setup-pocketbase.sh
+ssh user@$IP_BACKEND -p 4222 'bash -s' < ./code/setup-pocketbase.sh
 # echo "Install FRONTEND"
-# ssh user@$IP_FRONT -p 4222 'bash -s' < ./app.sh
+# ssh user@$IP_FRONT -p 4222 'bash -s' < ./code/app.sh
 # echo "Install UNAMI"
-# ssh user@$IP_UMAMI -p 4222 'bash -s' < ./install-docker.sh
+# ssh user@$IP_UMAMI -p 4222 'bash -s' < ./code/install-docker.sh
 # sleep 120
-# ssh user@$IP_UMAMI -p 4222 'bash -s' < ./unami.sh
+# ssh user@$IP_UMAMI -p 4222 'bash -s' < ./code/unami.sh
 # echo "Service pret a l'utilisation"
